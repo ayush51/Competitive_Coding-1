1128b2b52fb65b2188fff4f07c3b9bfe9c490f85
Uditkumar13 <Uditkumar2401@gmail.com>
2018-09-05 23:26:27 +0530

    optimal bst updated

--- DATA_STRUCTURES/TREES/optimal_binary_search_tree.cpp
+++ DATA_STRUCTURES/TREES/optimal_binary_search_tree.cpp
@@ -11,4 +11,29 @@ using namespace std ;
 int sum(int freq[], int i ,int j);
 // A recursive function to calculate the cost of optimal
 // binary search tree
-int opt
+int optCost(int freq[],int i ,int j)
+{// BASE CLASS
+  if (j<i) // no element in this subtree
+  return 0;
+  if (j==i) //  one element in yhis subtree
+  return freq[i];
+
+
+  // GET SUM OF freq[i],freq[i+1],......,freq[j]
+  int fsum=sum= sum(freq,i,j);
+
+  // INITIALIZE MINIMUM VALUES
+  int min= INT_MAX;
+  // ONE BY ONE CONSIDER ALL ELEMENTS AS ROOT AND RECURSIVELY FIND THE COST OF BST
+  // COMPARE THE COST WITH min AND UPDATE min IF needed
+  for ( int r =i;r<=j;++r)
+  {
+    int cost =optCost(freq,i,r-1) +optCost(freq,r+1,j);
+    if (cost<min){
+      min=cost;
+    }
+
+  }
+// RETURN minimum value 
+  return min + fsum;
+}
