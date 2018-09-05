e89a5923a54cc32c40de214ba0d3209d856c6760
Uditkumar13 <Uditkumar2401@gmail.com>
2018-09-05 23:03:52 +0530

    optimal binary search tree

---
+++ DATA_STRUCTURES/TREES/optimal_binary_search_tree.cpp
@@ -0,0 +1,14 @@
+// naive recursive implementation of optimal
+//binary search tree problem
+
+# include <iostream>
+#include <limits>
+using namespace std ;
+
+// a utility function to get sum of array elements
+//freq[i] to freq[j]
+
+int sum(int freq[], int i ,int j);
+// A recursive function to calculate the cost of optimal
+// binary search tree
+int opt
