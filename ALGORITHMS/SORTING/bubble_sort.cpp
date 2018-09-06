6a61e8c19b4a5662ebe37a4f00b9c0ca2b45a4ad
Uditkumar13 <Uditkumar2401@gmail.com>
2018-09-06 20:52:19 +0530

    modification in sort

--- ALGORITHMS/SORTING/bubble_sort.cpp
+++ ALGORITHMS/SORTING/bubble_sort.cpp
@@ -26,7 +26,7 @@ using namespace std;
  void printarray(int arr[],int size){
    int i;
    for (i=0;i<size;i++){
-         cout<< arr[i];
+         cout<< arr[i]<< " ";
    }
    cout << endl;
  }
@@ -34,8 +34,8 @@ using namespace std;
  int main(){
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr)/sizeof(arr[0]);
-    bubbleSort(arr, n);
+    bubblesort(arr, n);
     cout<<"SORTED array"<<endl;
-    printArray(arr, n);
+    printarray(arr, n);
     return 0;
 }
