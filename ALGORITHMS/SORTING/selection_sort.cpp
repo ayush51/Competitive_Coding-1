bfa66e752599b135318c447fc2bb26cbb6e3b24d
Uditkumar13 <Uditkumar2401@gmail.com>
2018-09-07 23:14:16 +0530

    selection_sort

--- ALGORITHMS/SORTING/selection_sort.cpp
+++ ALGORITHMS/SORTING/selection_sort.cpp
@@ -21,3 +21,38 @@ void print_array(int arr[],int n ){
   }
   cout<< endl;
 }
+
+// let us write, the main function for sorting in a selection way
+void selection_sort(int arr[],int n){
+  int i,j,min_idx;
+  // one by one move in unsorted boundary
+for (i=0;i<n;i++){
+// find min ele in unsorted array
+min_idx=i;
+for (j=i+1;j<n;j++){
+  if (arr[j]<arr[min_idx]){
+    min_idx=j;
+                           }
+                  }
+swap(arr[min_idx],arr[i]);
+                  }
+
+
+                                      }
+
+      // main function to see whether program works or not
+      int main(){
+
+        int arr[]={22,13,4,2,1,56,100,34,13};
+        int n=sizeof(arr)/sizeof(arr[0]);
+        cout<<"ARRAY BEFORE SORTING :  ";
+        print_array(arr,n);
+        cout<<endl;
+        // sorting
+        selection_sort(arr,n);
+        // print the print_array
+        cout<< "ARRAY AFTER SORTING :  ";
+
+        print_array(arr,n);
+        return 0;
+      }
