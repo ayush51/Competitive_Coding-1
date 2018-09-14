8f7d34cdabedd8a4bff0322866323bdf1befa32a
Uditkumar13 <Uditkumar2401@gmail.com>
2018-09-14 23:59:29 +0530

    bfs

--- ALGORITHMS/GRAPHS/breadth_first_search.cpp
+++ ALGORITHMS/GRAPHS/breadth_first_search.cpp
@@ -65,4 +65,18 @@ for (i=adj[s].begin();i!=adj[s].end();++i){
 
 }
 
-// driver 
+// program to implement the bfs
+int main(){
+  //create a graph , which u want to make
+  Graph g(4);
+  g.addEdge(0, 1);
+   g.addEdge(0, 2);
+   g.addEdge(1, 2);
+   g.addEdge(2, 0);
+   g.addEdge(2, 3);
+   g.addEdge(3, 3);
+   cout << "Following is Breadth First Traversal "
+       << "(starting from vertex 2) \n";
+  g.BFS(2);
+  return 0;
+}
