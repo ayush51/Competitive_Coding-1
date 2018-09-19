3e45dec4da58e8ee3f1938e56577855fd59bb636
Uditkumar13 <Uditkumar2401@gmail.com>
2018-09-19 23:48:19 +0530

    towerof_hanoi

--- ALGORITHMS/GREEDY-ALGORITHMS/tower0fhanoi.cpp
+++ ALGORITHMS/GREEDY-ALGORITHMS/tower0fhanoi.cpp
@@ -9,6 +9,19 @@ return ;
 }
 
 towerofhanoi(int n-1,from_rod,aux_rod,to_rod);
+cout<<"move disk "<<n<<"from rod "<<from_rod<<"to "<<to_rod;
 towerofhanoi(int n-1,aux_rod,to_rod,from_rod);
+
+}
+
+int main(){
+  int n;// no.of disk
+  cin>>n;
+  towerofhanoi(n,'A','B','C');// A,B AND C ARE NAMES OF THE RODS
+  return 0;
+
+}
+
+
 
 }
