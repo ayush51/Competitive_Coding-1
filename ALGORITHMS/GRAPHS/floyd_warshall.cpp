4ea5324a13813ede41df5cbc893bd9b5356155b1
Uditkumar13 <Uditkumar2401@gmail.com>
2018-09-22 23:42:09 +0530

    flyod_warshall

--- ALGORITHMS/GRAPHS/floyd_warshall.cpp
+++ ALGORITHMS/GRAPHS/floyd_warshall.cpp
@@ -31,9 +31,64 @@ void floyd_warshall(int Graph[][V]){
     }
   }
 
+
+// for updating the weight matrix we will consider the intermediate values
+// in doing so, we check the current dist weight of dist[i][j] ,
+// if this current weight tends to be greater than the intermediate summationn of weights
+// dist[i][k] +dist[k][j] only then we optimize our distance value
+// optimizating values are done as follows
+
+for (int k=0;k<V;k++){
+  for (int i =0;i<V;i++){
+    for (int j=0;j<V;j++){
+
+      if (dist[i][j]>dist[i][k] +dist[k][j]){
+        dist[i][j]=dist[i][k]+dist[k][j];
+
+      }
+    }
+  }
+}
+
+print_solution(dist);
+}
+// defining the  body of print_solution function
+void print_solution(int dist[][V]){
+
+  cout<<"FOLLOWING IS THE SMALLEST DISTANCE POSSIBLE IN THE
+  CONNECTED EDGES ,EVERY CONNECTED VERTICES \n";
+
+  for (int i=0;i<V;i++){
+    for (int j=0;j<V;j++){
+      if (dist[i][j]==INF)
+      cout<< "INF"<<" ";
+      else
+        cout<<dist[i][j];
+    }
+    cout<<endl;
+  }
+
 }
 
 
+// main function in implementing the warshall flyod algorithm
 
+int main(){
 
-}
+  '''Let us create the following weighted graph
+            10
+       (0)------->(3)
+        |         /|\
+      5 |          |
+        |          | 1
+       \|/         |
+       (1)------->(2)
+            3           '''
+  int graph[V][V] = { {0,   5,  INF, 10},
+                      {INF, 0,   3, INF},
+                      {INF, INF, 0,   1},
+                      {INF, INF, INF, 0}
+                    };
+
+flyod_warshall(graph);
+                    return 0;}
