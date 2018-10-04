07f43b0e6298e085c795efcc4b84b8126d1077d8
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-04 23:52:27 +0530

    n stairs user defined

--- BASIC_PROGRAMMING/RECURSION/N_step_problem_user choice.cpp
+++ BASIC_PROGRAMMING/RECURSION/N_step_problem_user choice.cpp
@@ -9,20 +9,22 @@ return 1;
 }
 
 int ans=0;
-if (n>=1 && i>=1){
-  i--;
-ans=step(n-i);
+if (n>=1){
+for (int j=1;j<=n && j<=i; j++){
+ans+=step(n-j,i);
+                              }
 }
 return ans;
-
 }
 
 int main(){
-
 int n;
 // no of.steps
 cin>>n;
-
-cout<<step(n);
+int i;
+// no of steps user wnat to choose
+cin>>i;
+cout<<"number of total solution to this problem using i starirs is : "<<" " ;
+cout << step(n,i);
 return 0;
 }
