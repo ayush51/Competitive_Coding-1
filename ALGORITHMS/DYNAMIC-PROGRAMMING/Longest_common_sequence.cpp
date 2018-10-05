5b1f6bc23c9e2de034350cfd2edca00df9b4cec5
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-05 23:53:44 +0530

    lcs

--- ALGORITHMS/DYNAMIC-PROGRAMMING/Longest_common_sequence.cpp
+++ ALGORITHMS/DYNAMIC-PROGRAMMING/Longest_common_sequence.cpp
@@ -3,7 +3,19 @@ using namespace std;
 // it will find the length of common sequence
  int lcs(char *X,char *Y,int m, int n){
 int L[m+1][n+1];it will store our common length trick
+int i,j;
 
+ //following steps build the L[m+1][n+1] in bottom up fashion
+ // and it keep trsck of the longest common sequence
+
+ for(i=0;i<=m;i++){
+   for (j=0;j<=n;j++)
+   {
+     if (i==0 || j==0){
+       L[i][j]=0;// making the first row and first column all elements equal to zero
+     }
+   }
+ }
 
 
   }
