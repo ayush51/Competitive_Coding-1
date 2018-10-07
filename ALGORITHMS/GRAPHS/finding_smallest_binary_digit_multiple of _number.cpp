a68a49e9811c24400ae49c7fc7e6148136c3541a
Uditkumar13 <Uditkumar2401@gmail.com>
2018-10-07 22:13:21 +0530

    is binary or not

--- ALGORITHMS/GRAPHS/finding_smallest_binary_digit_multiple of _number.cpp
+++ ALGORITHMS/GRAPHS/finding_smallest_binary_digit_multiple of _number.cpp
@@ -3,3 +3,16 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
+
+// now let us make a function to know whether a number is a
+// binary number or not
+bool Isbinary_num(int N){
+
+  while(N>=0){
+int digit =N%10;
+if (digit!=0 || digit 1)
+   return false;
+N=N/10;
+  }
+  return true;
+}
